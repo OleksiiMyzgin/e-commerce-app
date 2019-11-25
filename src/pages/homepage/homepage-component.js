ccb5051e1fa99bddd5ed13f7e9062ec674950b2c
Alexey <mr.oleksii5@gmail.com>
2019-11-25 16:59:56 +0200

    finish directory components

--- src/pages/homepage/homepage-component.js
+++ src/pages/homepage/homepage-component.js
@@ -1,45 +1,12 @@
 import React from "react";
 
+import Directory from "../../components/directory/directory.component";
+
 import "./homepage.styles.scss";
 
 const HomePage = () => (
 	<div className="homepage">
-		<div className="directory-menu">
-			<div className="menu-item">
-				<div className="content">
-					<h1 className="title">Hats</h1>
-					<span className="sub-title">Shop now</span>
-				</div>
-			</div>
-
-			<div className="menu-item">
-				<div className="content">
-					<h1 className="title">Hats</h1>
-					<span className="sub-title">Shop now</span>
-				</div>
-			</div>
-
-			<div className="menu-item">
-				<div className="content">
-					<h1 className="title">Hats</h1>
-					<span className="sub-title">Shop now</span>
-				</div>
-			</div>
-
-			<div className="menu-item">
-				<div className="content">
-					<h1 className="title">Hats</h1>
-					<span className="sub-title">Shop now</span>
-				</div>
-			</div>
-
-			<div className="menu-item">
-				<div className="content">
-					<h1 className="title">Hats</h1>
-					<span className="sub-title">Shop now</span>
-				</div>
-			</div>
-		</div>
+		<Directory />
 	</div>
 );
 
