c9fc56ea1951d90193a4b88d626e22bae419c695
Alexey <mr.oleksii5@gmail.com>
2019-12-03 17:50:59 +0200

    add custom button and styles for sign-in component

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 import "./sign-in.styles.scss";
 
 class SignIn extends React.Component {
@@ -28,7 +29,7 @@ class SignIn extends React.Component {
 	render() {
 		return (
 			<div className="sing-in">
-				<h2>I already have an account</h2>
+				<h2 className="title">I already have an account</h2>
 				<span> Sign in with your email and password</span>
 
 				<form onSubmit={this.handleSubmit}>
@@ -49,7 +50,7 @@ class SignIn extends React.Component {
 						required
 					/>
 
-					<input type="submit" value="Submit Form" />
+					<CustomButton type="submit">Sign in</CustomButton>
 				</form>
 			</div>
 		);
