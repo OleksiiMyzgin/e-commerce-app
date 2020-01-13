8c58e35c9bf6fabdc05021546f598e51fc0a06b1
Alexey <mr.oleksii5@gmail.com>
2020-01-13 13:32:51 +0200

    added redux-saga and added sagas for shop component

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,34 @@
+import { takeLatest, call, put } from "redux-saga/effects";
+
+import {
+	firestore,
+	convertCollectionsSnapshotToMap
+} from "../../firebase/firebase.utils";
+
+import {
+	fetchCollectionsSuccess,
+	fetchCollectionsFailure
+} from "./shop.actions";
+
+import shopActonTypes from "./shop.types";
+
+export function* fetchCollectionsAsync() {
+	try {
+		const collectionRef = firestore.collection("collections");
+		const snapshot = yield collectionRef.get();
+		const collectionsMap = yield call(
+			convertCollectionsSnapshotToMap,
+			snapshot
+		);
+		yield put(fetchCollectionsSuccess(collectionsMap));
+	} catch (err) {
+		yield put(fetchCollectionsFailure(err));
+	}
+}
+
+export function* fetchCollectionsStart() {
+	yield takeLatest(
+		shopActonTypes.FETCH_COLLECTIONS_START,
+		fetchCollectionsAsync
+	);
+}
