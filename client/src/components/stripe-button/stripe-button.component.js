2eabe9b2df60cc621b5346ed681315b2decb74dd
Alexey <mr.oleksii5@gmail.com>
2020-01-15 11:42:12 +0200

    connecting client to server

--- client/src/components/stripe-button/stripe-button.component.js
+++ client/src/components/stripe-button/stripe-button.component.js
@@ -1,13 +1,29 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
 	const priceForStripe = price * 100;
 	const publishableKey = "pk_test_7I8KGVYOZtTMUPgnye9Cj1Tz00vCcdQAcv";
 
 	const onToken = token => {
-		console.log(token);
-		alert("Payment Successful");
+		axios({
+			url: "payment",
+			method: "post",
+			data: {
+				amount: priceForStripe,
+				token
+			}
+		})
+			.then(res => {
+				alert("Payment successful");
+			})
+			.catch(err => {
+				console.error("Payment error: ", JSON.parse(err));
+				alert(
+					"There was an issue with your payment. Please sure you use the provided credit cart"
+				);
+			});
 	};
 
 	return (
