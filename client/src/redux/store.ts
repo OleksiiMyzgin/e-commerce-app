4161242ce1636831458d20bd888449313b494ed2
OleksiiMyzgin <oleksii.myzgin@dentsplysirona.com>
2020-04-15 11:15:25 +0300

    store refactoring

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -8,10 +8,10 @@ import rootSaga from "./root-saga";
 
 const sagaMiddleware = createSagaMiddleware();
 
-const middlewares = [sagaMiddleware];
+const middlewares: any[] = [sagaMiddleware];
 
 if (process.env.NODE_ENV === "development") {
-	middlewares.push(logger);
+  middlewares.push(logger);
 }
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
